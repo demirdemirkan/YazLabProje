2ad882ad8f7dc6661c313e69ee74589ebd8e0a9d
efeyilmazzz <efeylmz2604@gmail.com>
2025-10-17 21:42:26 +0300

    Yürüyüş mekaniğini ve animasyonunu ekledim.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    public float walkSpeed = 2f;
+    public float sprintSpeed = 5f;
+
+    Rigidbody rb;
+    private bool isCharacterWalking;
+    public Animator animator;
+    Vector3 inputDir;
+    float currentSpeed;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
+        rb.constraints = RigidbodyConstraints.FreezeRotationX
+                       | RigidbodyConstraints.FreezeRotationY
+                       | RigidbodyConstraints.FreezeRotationZ;
+    }
+
+    void Update()
+    {
+        currentSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed;
+
+        inputDir = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputDir += transform.forward;
+            //TriggerWalkAnimation();
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputDir -= transform.forward;
+            //TriggerWalkAnimation();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputDir -= transform.right;
+            //TriggerWalkAnimation();
+        }
+        if (Input.GetKey(KeyCode.D)) 
+        { inputDir += transform.right;
+           // TriggerWalkAnimation();
+        }
+        // if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A)&&!Input.GetKey(KeyCode.D)) 
+        //{
+        //  TriggerIdleAnimation();
+        //}
+        if (inputDir.magnitude < .1f) {TriggerIdleAnimation();}
+        else {TriggerWalkAnimation();}
+        inputDir = inputDir.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        // Y bileşenini koru (yerçekimi için)
+        Vector3 v = rb.velocity;
+
+        // Hedef yatay hız
+        Vector3 targetXZ = inputDir * currentSpeed;
+
+        // Anlık hızda XZ’yi hedefe ayarla, Y’yi dokunma
+        v.x = targetXZ.x;
+        v.z = targetXZ.z;
+        rb.velocity = v;
+
+        
+    }
+     void TriggerWalkAnimation() 
+    {
+        if(!isCharacterWalking)
+        {
+            animator.SetTrigger("Walk");
+            isCharacterWalking = true;
+        }
+    }
+    void TriggerIdleAnimation() 
+    {
+        if (isCharacterWalking) 
+        {
+            animator.SetTrigger("Idle");
+            isCharacterWalking = false;
+        }
+    }
+}
